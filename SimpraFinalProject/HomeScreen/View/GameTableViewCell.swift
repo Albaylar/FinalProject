4bd8ed73f1e57aab5f6575fe6d95307b422dccb3
Albaylar <furkan.albaylar@isik.edu.tr>
2023-01-24 14:10:36 +0300

    Cells
    Cell's are created

---
+++ SimpraFinalProject/HomeScreen/View/GameTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  GameTableViewCell.swift
+//  SimpraFinalProject
+//
+//  Created by Furkan Deniz Albaylar on 24.01.2023.
+//
+
+import UIKit
+
+class GameTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
