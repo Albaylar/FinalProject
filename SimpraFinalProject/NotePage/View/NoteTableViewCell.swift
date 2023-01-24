e521f245b03e4e8fcaddf796c80bac7e04b4d963
Albaylar <furkan.albaylar@isik.edu.tr>
2023-01-24 16:46:01 +0300

    Connectivity
    NoteTableViewCell is designed and connected with ModelView

--- SimpraFinalProject/NotePage/View/NoteTableViewCell.swift
+++ SimpraFinalProject/NotePage/View/NoteTableViewCell.swift
@@ -2,13 +2,18 @@
 //  NoteTableViewCell.swift
 //  SimpraFinalProject
 //
-//  Created by Furkan Deniz Albaylar on 24.01.2023.
+//  Created by Furkan Deniz Albaylar on 23.01.2023.
 //
 
 import UIKit
 
+
 class NoteTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var noteDetail: UITextView!
+    @IBOutlet weak var noteTitle: UILabel!
+    @IBOutlet weak var gameTitle: UILabel!
+    @IBOutlet weak var containerView: UIView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -19,5 +24,12 @@ class NoteTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    //MARK: - Global Functions
+    func configureCell(_ note:Note){
+        gameTitle.text = note.gameTitle
+        noteTitle.text = note.noteTitle
+        noteDetail.text = note.noteDetail
+    }
     
+
 }
