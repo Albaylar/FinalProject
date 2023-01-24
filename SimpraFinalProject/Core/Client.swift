0e1b612823993ee6f12f2690c16e101e5239eba2
Albaylar <furkan.albaylar@isik.edu.tr>
2023-01-24 12:27:13 +0300

    DataFetch
    getPopularGames are updated.

---
+++ SimpraFinalProject/Core/Client.swift
@@ -0,0 +1,26 @@
+//
+//  Client.swift
+//  LegendaryApp
+//
+//  Created by Furkan Deniz Albaylar on 22.01.2023.
+//
+
+import Foundation
+import Alamofire
+
+struct Headers {
+    
+    static let apiKey = "c219f28a485d4caeaf74e4116b310e0e"
+}
+
+
+final class RawgClient {
+    static let baseUrl = "https://rawg.io/api/games"
+    static let apiUrl = "https://api.rawg.io/api/games"
+    
+    static func getPopularGames(completion: @escaping ([RawgModel]?, Error?) -> Void) {
+        let urlString = baseUrl + "/lists/main?discover=true&ordering=-relevance&page_size=40&page=1" + "&key=" + Headers.apiKey
+        handleResponse(urlString: urlString, responseType: GetRawgResponseModel.self) { responseModel, error in
+            completion(responseModel?.results, error)
+        }
+    }
